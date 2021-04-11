e3ffccea416a672e724238c95a22540993e60628
kittybagpipes <sparticus978@gmail.com>
2021-04-11 08:15:28 -0700

    functional programming principles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import { compose, pipe } from "lodash/fp";
 
-function sayHello() {
-  return function () {
-    return "Hello World ";
-  };
-}
-
 // Higher order functions either take a function as an argument or returns a function.
 
 let input = "  Javascript  ";
@@ -13,9 +7,29 @@ let input = "  Javascript  ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-const wrapInDiv = (str) => `<div>${str}</div>`;
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
-const result = wrapInDiv(toLowerCase(trim(input)));
+const transform = pipe(trim, toLowerCase, wrap("span"));
+console.log(transform(input));
+
 // trim
 //wrapInDiv
+
+const person = {
+  name: "john",
+  address: {
+    country: "USA",
+    city: "San Francisco",
+  },
+};
+const updated = {
+  ...person,
+  address: {
+    ...person.address,
+    city: "New York",
+  },
+  name: "Bob",
+};
+
+console.log(person);
