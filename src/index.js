694b3e47995a1db7648563ea9802bedd026168ea
kittybagpipes <sparticus978@gmail.com>
2021-04-11 05:54:50 -0700

    added lodash

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+
+function sayHello() {
+  return function () {
+    return "Hello World ";
+  };
+}
+
+// Higher order functions either take a function as an argument or returns a function.
+
+let input = "  Javascript  ";
+
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
+
+const result = wrapInDiv(toLowerCase(trim(input)));
+// trim
+//wrapInDiv
